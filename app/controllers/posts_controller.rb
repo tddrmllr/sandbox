fa6f1048d7d1cb5a5c88cf4f9245bfd403bed16b
violentj <miller.todd@live.com>
2012-09-21 14:12:16 -0600

    got post ajax working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,26 @@
 class PostsController < ApplicationController
   
-  respond_to :html, :js, :xml, :json
+  respond_to :html, :js, :xml
   
   def index
     @title = 'posts'
     @header = 'posts'
-    @posts = Post.all
+    @posts = Post.all.sort_by{|t| - t.created_at.to_i}
     @post = Post.new
   end
   
   def create
-    @post = Post.new(params[:post])
-    respond_with do |format|
-      if @post.save
-        format.html {redirect_to @post}
-        format.js
-       else
-         format.html {render :action => 'new'}
-      end
-    end
+    @new_post = Post.new(params[:post])
+    @new_post.save
+    @updated_posts = Post.all.sort_by{|t| - t.created_at.to_i}
+    respond_with(@updated_posts)
   end
   
   def destroy
     @post = Post.find(params[:id])
+    @post_id = @post.id
     @post.destroy
-    respond_with(posts_url) do |format|
-      format.html
-      format.js
-    end
+    respond_with(@post_id)
   end
   
 end
